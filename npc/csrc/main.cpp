41203a7f89c0a59f7d9e6e564cc6288e75e1943e
Maricaya <915270549@qq.com>
2025-01-26 22:29:12 -0600

    feat: npc

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char **argv) {
+    Verilated::commandArgs(argc, argv);
+    Vtop* top = new Vtop;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("wave.vcd");
+
+    for (int i = 0; i < 10; i++) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+
+        top->a = a;
+        top->b = b;
+        top->eval();
+
+        tfp->dump(i * 10);
+
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+    }
+
+    tfp->close();
+    delete tfp;
+    delete top;
+    return 0;
 }
